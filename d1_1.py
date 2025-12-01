27706e677a22fd63cd3f0008e3fe0a8c6ca6ac8e
Macieksep <macieksepeta@gmail.com>
2025-12-01 16:38:08 +0100

    1

---
+++ d1_1.py
@@ -0,0 +1,28 @@
+file = open("resources/f1_1", "r")
+
+curr = 50
+
+passwd = 0
+
+for i in file:
+
+    currN = int(i[1:])
+
+    if currN >= 100:
+        currN -= 100 * (currN // 100)
+
+    if i[0] == 'L':
+        curr -= currN
+
+        if curr < 0:
+            curr += 100*(currN//100+1)
+    else:
+        curr += currN
+
+        if curr > 99:
+            curr -= 100*(currN//100+1)
+
+    if curr == 0:
+        passwd += 1
+
+print(passwd)
---
+++ d1_2.py
@@ -0,0 +1,40 @@
+file = open("resources/f1_1", "r")
+
+curr = 50
+
+clicks = 0
+
+for i in file:
+
+    skipClick = False
+
+    print(i)
+
+    currN = int(i[1:])
+
+    if currN >= 100:
+        clicks += (currN // 100)
+        currN -= 100 * (currN // 100)
+
+    if i[0] == 'L':
+
+        curr -= currN
+
+        if curr < 0:
+            if curr + currN != 0:
+                clicks += (currN//100+1)
+                skipClick = True
+            curr += 100*(currN//100+1)
+
+    else:
+        curr += currN
+
+        if curr > 99:
+            clicks += (currN//100+1)
+            curr -= 100*(currN//100+1)
+            skipClick = True
+
+    if curr == 0 and skipClick == False:
+        clicks += 1
+
+print("\n",clicks)
