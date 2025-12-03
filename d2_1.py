8f54171d1d7efd2300d7d163221caccec351c3ed
Macieksep <macieksepeta@gmail.com>
2025-12-03 17:12:54 +0100

    2

---
+++ d2_1.py
@@ -0,0 +1,26 @@
+def is_correct(num) -> bool:
+
+    if len(num) % 2 == 0:
+        if num[:len(num) // 2] == num[len(num) // 2:]:
+            return False
+
+    return True
+
+rgs = (
+        open("resources/f2_1", "r")
+       .read()
+       .split(',')
+       )
+
+result = 0
+
+for rg in rgs:
+
+
+    rgBorder = rg.split('-')
+
+    for numId in range(int(rgBorder[0]), int(rgBorder[1])+1):
+        if not is_correct(str(numId)):
+            result += numId
+
+print(result)
